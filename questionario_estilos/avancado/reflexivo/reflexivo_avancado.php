3333efefd963699484147abea84762ea117017a5
Myke Oliveira <38540828+mykeoliveira@users.noreply.github.com>
2020-06-11 18:19:51 -0300

    Update reflexivo_avancado.php

--- questionario_estilos/avancado/reflexivo/reflexivo_avancado.php
+++ questionario_estilos/avancado/reflexivo/reflexivo_avancado.php
@@ -52,26 +52,41 @@
                     
                   <div class="line"></div> 
 
-                  <h2>Fluxograma e Pseudocódigo</h2> 
-                  <br />Link de acesso: <a target="_blank" href="https://www.oficinadanet.com.br/artigo/desenvolvimento/como_fazer_um_fluxograma">Tutorial Fluxograma</a>, <a target="_blank" href="https://www.embarcados.com.br/pseudocodigo/">Tutorial Pseudocódigo</a><br /><BR />
+                 <h2>Fluxograma</h2> 
+                  <br />Link de acesso: <a target="_blank" href="https://www.oficinadanet.com.br/artigo/desenvolvimento/como_fazer_um_fluxograma">Tutorial com Diagramas</a><br />
+                  Link de acesso: <a target="_blank" href="https://www.youtube.com/watch?v=orSLpujvsaA"> Vídeo </a><br><br>
                   
-                 <h2>VisuAlg</h2> 
-                 <br />Link de acesso: <a target="_blank" href="http://www.devmedia.com.br/introducao-ao-visualg/25748">Tutorial com Diagramas </a><br /><BR />
+                  <h2> Pseudocódigo</h2>
+                  <br> Link de acesso: <a target="_blank" href="https://www.embarcados.com.br/pseudocodigo/"> Tutorial com Diagramas </a>
+                  <br> Link de acesso: <a target="_blank" href="https://www.youtube.com/watch?v=PZhU0XRrleY"> Vídeo </a><br><br>
+                 <h2>VisuAlg</h2> <br>
+                 Link de acesso: <a target="_blank" href="http://www.devmedia.com.br/introducao-ao-visualg/25748">Tutorial com Diagramas </a><br />
+                 Link de acesso: <a target="_blank" href="https://www.youtube.com/watch?v=XS5wkYJAY6s"> Vídeo </a><br><br>
                  
-                 <h2>Teste de Mesa</h2> <br />Link de acesso: <a target="_blank" href="https://pt.slideshare.net/henriquecarmona/aula-4-teste-de-mesa">Tutorial com Diagramas </a><br /><br />
+                 <h2>Teste de Mesa</h2> <br />Link de acesso: <a target="_blank" href="https://pt.slideshare.net/henriquecarmona/aula-4-teste-de-mesa">Tutorial com Diagramas </a><br />
+                 Link de acesso: <a target="_blank" href="https://www.youtube.com/watch?v=2lNCLHzTVIE"> Vídeo </a><br><br>
                  
 				 <h2>Linguagem C</h2> <br />
+                 Introdução a linguagem C: <a target="_blank" href="https://www.youtube.com/watch?v=tnGBpO0O7PA"> Vídeo </a><br>
 				Operadores Lógicos: <a target="_blank" href="http://mtm.ufsc.br/~azeredo/cursoC/aulas/c360.html">Tutorial com Diagramas </a> <br />
                 Funções de Entrada: <a target="_blank" href="https://pt.wikibooks.org/wiki/Programar_em_C/Entrada_e_sa%C3%ADda_simples"> Tutorial com Diagramas </a> <br />
                 Funções de Saída: <a target="_blank" href="https://pt.wikibooks.org/wiki/Programar_em_C/Entrada_e_sa%C3%ADda_simples"> Tutorial com Diagramas </a> <br />
                 Processo de atribuição à variáveis: <a target="_blank" href="http://linguagemc.com.br/variaveis-em-linguagem-c/"> Tutorial com Diagramas </a> <br />
-				Estrutura if-else: <a target="_blank" href="http://linguagemc.com.br/estruturas-de-decisao-encadeadas-if-else-if-else/"> Tutorial com Diagramas </a> <br />
-				While: <a target="_blank" href="http://linguagemc.com.br/o-comando-while-em-c/"> Tutorial com Diagramas </a><br />
-				For: <a target="_blank" href="http://linguagemc.com.br/a-estrutura-de-repeticao-for-em-c/"> Tutorial com Diagramas </a> <br />
-				Vetores: <a target="_blank" href="http://linguagemc.com.br/vetores-ou-arrays-em-linguagem-c/"> Tutorial com Diagramas</a><br /><br />
+				Estrutura if-else: <a target="_blank" href="https://www.youtube.com/watch?v=xKq2aLAflx0"> Vídeo </a>, <a target="_blank" href="http://linguagemc.com.br/estruturas-de-decisao-encadeadas-if-else-if-else/"> Tutorial com Diagramas </a> <br />
+				While: <a target="_blank" href="https://www.youtube.com/watch?v=rCFh-tvoXIc"> Vídeo </a>,<a target="_blank" href="http://linguagemc.com.br/o-comando-while-em-c/"> Tutorial com Diagramas </a><br />
+				For: <a target="_blank" href="https://www.youtube.com/watch?v=lRuQ8S-jXcY0"> Vídeo </a>,<a target="_blank" href="http://linguagemc.com.br/a-estrutura-de-repeticao-for-em-c/"> Tutorial com Diagramas </a> <br />
+				Vetores: <a target="_blank" href="https://www.youtube.com/watch?v=saH9wHeo5u0"> Vídeo </a>,<a target="_blank" href="http://linguagemc.com.br/vetores-ou-arrays-em-linguagem-c/"> Tutorial com Diagramas</a><br /><br />
                 
-                Se preferir, aqui está disponível um link com um livro completo de Algoritmos. Basta clicar <a target="_blank" href="https://docs.google.com/file/d/0ByLDgIKIkMi2MTc0MDJmOWItNTQ1Yy00NTczLWI0OTQtNjA1NDI5YTllYzZm/edit?hl=en"> aqui </a>.<br/><br>
-                <form>
+                <h2>Livros</h2>
+		<br> Estudo Dirigito: Algoritmos <a target="_blank" href="https://docs.google.com/file/d/0ByLDgIKIkMi2MTc0MDJmOWItNTQ1Yy00NTczLWI0OTQtNjA1NDI5YTllYzZm/edit?hl=en"> acessar </a>.
+		<br> Linguagem C <a target="_blank" href="https://www.ime.usp.br/~slago/slago-C.pdf"> acessar </a>.
+		<br> Computação Científica em Linguagem C <a target="_blank" href="https://www.ufrgs.br/reamat/ComputacaoCientifica/livro/livro.pdf"> acessar </a>.<br><br>
+		
+		<h2>Artigos</h2>
+		<br> Estilo de código - Boas práticas de programação em linguagem C <a target="_blank" href="https://www.embarcados.com.br/boas-praticas-de-programacao-em-linguagem-c/"> acessar </a>.
+		<br> Especificações da Linguagem C <a target="_blank" href="https://www.mentebinaria.com.br/artigos/especificações-da-linguagem-c-r17/"> acessar </a><br><br>
+		   
+		    <form>
 					<input type="button" value="Voltar" onClick="JavaScript: window.history.back();">
 				</form>   
             
